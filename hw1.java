c01ae32e7916f3e2d117016555db89ffbe16345a
Abhinav <abhinavmanu0042@gmail.com>
2026-08-07 12:45:52 +0530

    Add smallshop class with main method for calculating total price of items

---
+++ hw1.java
@@ -0,0 +1,14 @@
+package javamy;
+public class smallshop{
+	public static void main(String[] args) {
+		String item="soap";
+		int quantity=4;
+		double price=18.75;
+		double total= (quantity*price);
+		System.out.println("the item is "+item);
+		System.out.println("quantity "+quantity);
+		System.out.println("unit price per piece "+price);
+		System.out.println("the total price is "+total);
+	}
+}
+
