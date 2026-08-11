3e52b294926a29c9e7973e8a2b9335af84e4ea9a
Abhinav <abhinavmanu0042@gmail.com>
2026-08-11 11:17:01 +0530

    Add mainpgm class with main method to initialize greeting and user profile

---
+++ hw2.java
@@ -0,0 +1,17 @@
+package javamy;
+import greetings.greetingdisplay;
+import profile.userprofile;
+
+public class mainpgm{
+	public static void main(String[] args) { 
+		greetingdisplay obj1=new greetingdisplay();
+		userprofile obj2=new userprofile();
+		obj2.methodone();
+		obj1.methodtwo();
+		
+
+		
+		
+		
+	}
+}
